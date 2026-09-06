f7339d0355d03967116d6c0de1b63a0fc33efed2
maikapa <lodilodsn@gmail.com>
2026-09-06 15:38:35 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Legt die App beim ersten Aufruf ab. Danach startet sie auch ohne Netz. */
-var CACHE = "detektivbuero-v6";
+var CACHE = "detektivbuero-v7";
 var DATEIEN = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function (e) {
