17b68f1677147d7906e36bbe48562c3ad6c06936
maikapa <lodilodsn@gmail.com>
2026-09-07 08:16:31 +0200

    Add files via upload

---
+++ sw-1.js
@@ -0,0 +1,24 @@
+/* Legt die App beim ersten Aufruf ab. Danach startet sie auch ohne Netz. */
+var CACHE = "detektivbuero-v8";
+var DATEIEN = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", function (e) {
+  e.waitUntil(caches.open(CACHE).then(function (c) { return c.addAll(DATEIEN); }));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (e) {
+  e.waitUntil(caches.keys().then(function (namen) {
+    return Promise.all(namen.filter(function (n) { return n !== CACHE; })
+      .map(function (n) { return caches.delete(n); }));
+  }));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (treffer) {
+      return treffer || fetch(e.request);
+    })
+  );
+});
